cecca23ade6ce9d80b203f23f98aec649ca645ac
Zech Thurman <zechariah.thurman@gmail.com>
2022-08-09 20:34:06 -0600

    First commit so that I can pull to the IOC for my demo

---
+++ README.md
@@ -0,0 +1,24 @@
+## reactorsim
+
+The intent of this script is to be run every minute with cron 
+on a linux based [EPICS]{https://docs.epics-controls.org/en/latest/index.html} 
+SoftIOC. The simulation covers some of the signals associated 
+with providing a reactor trip for a four loop pressurized 
+water reactor. 
+
+For those who like reading lots of pdfs about the arcane 
+practice of building a PWR and implementing reactor trip
+and ESFAS features the source information is available 
+on the NRCs site at: https://www.nrc.gov/docs/ML1122/.
+
+I should probably make another repo with all of the documents
+on github for posterity when they notice those files are 
+outward facing.
+
+Pressurizer pressure low and high trip: 1925 psig low sp, 2375 psig high sp; 1700-2500 psig
+Pressurizer level high trip: 0-100%, 90% high sp
+Reactor Coolant Loop Low Flow trip: 0-100%, 20% low flow setpoint
+Steam generator level low low trip: 0-100%, 15% low low sp
+turbine trip
+All go into reactor trip, active low
+'''
---
+++ reactorsim.py
@@ -0,0 +1,55 @@
+import random
+from subprocess import Popen, PIPE
+
+
+random.seed()
+
+
+def get_signal_value(signal_low, signal_high):
+    return random.uniform(signal_low, signal_high)
+
+
+class ProcessValue:
+    def __init__(self, name, signal_low, signal_high):
+        self.name = name
+        self.value = get_signal_value(signal_low=signal_low, signal_high=signal_high)
+
+
+simulator_spec = {
+    "przlvl": ProcessValue(name="pressurizer:level", signal_low=0.0, signal_high=5.0),
+    "przprs": ProcessValue(
+        name="pressurizer:pressure", signal_low=0.01, signal_high=0.15
+    ),
+    "rc1flow": ProcessValue(
+        name="reactorcoolant:flow1", signal_low=0.01, signal_high=0.15
+    ),
+    "rc2flow": ProcessValue(
+        name="reactorcoolant:flow2", signal_low=0.01, signal_high=0.15
+    ),
+    "rc3flow": ProcessValue(
+        name="reactorcoolant:flow3", signal_low=0.01, signal_high=0.15
+    ),
+    "rc4flow": ProcessValue(
+        name="reactorcoolant:flow4", signal_low=0.01, signal_high=0.15
+    ),
+    "sg1lvl": ProcessValue(
+        name="steamgenerator:level1", signal_low=2.5, signal_high=10.0
+    ),
+    "sg2lvl": ProcessValue(
+        name="steamgenerator:level2", signal_low=2.5, signal_high=10.0
+    ),
+    "sg3lvl": ProcessValue(
+        name="steamgenerator:level3", signal_low=2.5, signal_high=10.0
+    ),
+    "sg4lvl": ProcessValue(
+        name="steamgenerator:level4", signal_low=2.5, signal_high=10.0
+    ),
+}
+
+
+def simulate():
+    for each_device, each_pv in simulator_spec.items():
+        Popen(["caput", each_pv.name, each_pv.value], stdout=PIPE)
+
+
+simulate()
