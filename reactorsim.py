2043a188fed27c2cd0c444f40bd5a91249db2676
Zech Thurman <zechariah.thurman@gmail.com>
2022-08-09 20:39:59 -0600

    squashin' bogs?

--- reactorsim.py
+++ reactorsim.py
@@ -49,7 +49,7 @@ simulator_spec = {
 
 def simulate():
     for each_device, each_pv in simulator_spec.items():
-        Popen(["caput", each_pv.name, each_pv.value], stdout=PIPE)
+        Popen(["caput", each_pv.name, f"{each_pv.value}"], stdout=PIPE)
 
 
 simulate()
